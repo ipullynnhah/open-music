ff4594b3e73832feda3aa6b8edfa9fcdb1b148f2
ipullynnhah <ipullynnhah@gmail.com>
2024-04-05 21:28:23 -0300

    Tarefa 3 concluída

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,30 @@
 import { applyInputRangeStyle } from "./inputRange.js";
+import {albumList} from "./albumsDatabase.js";
+
+function createAlbumCard({ title, genre, band, price, img }) {
+  return `<li>
+  <img src="${img}" alt="${title}"/>
+  <div class="card-container">
+    <h3>${title}</h3>
+    <div>
+      <p class="band">${band}</p>
+      <p class="genre">${genre}</p>
+    </div>
+
+    <div>
+      <p class="price">R$ ${price.replace(".", ",")}</p>
+      <button>Comprar</button>
+    </div>
+  </div>
+</li>
+`;
+}
+
+function renderAlbums(albums) {
+  genreUl.innerHTML = albums.reduce((ac, album)=> ac + createAlbumCard(album), "");
+}
+
+const genreUl = document.querySelector(".albums ul");
 
 applyInputRangeStyle();
+renderAlbums(albumList);
